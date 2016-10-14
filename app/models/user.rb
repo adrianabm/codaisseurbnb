57f5a3a44ecbcaa65663174a1e217cf0de935252
adrianabm <adrianabm@gmail.com>
2016-10-14 10:43:15 +0200

    Through Association: Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :rooms
   has_many :rooms, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :booked_rooms, through: :bookings, source: :room
   has_one :profile
 
   def has_profile?
